f5f886dd1de1cbf77af4939268d85901e160add7
Svetlana <fidelity-67rus@mail.ru>
2022-10-07 15:47:19 +0300

    feat: add burger menu

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,26 @@
+/*menu hamburger*/
+
+const burgerItem = document.querySelector('.hamburger');
+const menu = document.querySelector('.burger-menu');
+const menuCloseItem = document.querySelector('.hamburger-close');
+const screenDimming = document.querySelector('.hamburger-screen-dimming');
+const burgerLinks = document.querySelectorAll('.burger-nav-link');
+
+burgerItem.addEventListener('click', () => {
+  menu.classList.add('burger-menu-active');
+  screenDimming.classList.add('hamburger-screen-dimming-active');
+  document.body.style.overflow = 'hidden';
+});
+const closeMenu = () => {
+  menu.classList.remove('burger-menu-active');
+  screenDimming.classList.remove('hamburger-screen-dimming-active');
+  document.body.style.overflow = '';
+};
+
+menuCloseItem.addEventListener('click', closeMenu);
+  
+screenDimming.addEventListener('click', closeMenu);
+
+for (let i = 0; i < burgerLinks.length; i += 1) {
+  burgerLinks[i].addEventListener('click', closeMenu);
+}
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,3 +1,27 @@
-// (function () {
-//   alert('Доброго времени суток!\nБольшая просьба при проверки на Perfect Pixel переключиться на Mobile в Toolbar.\nА то Descktop добавляет скрол и +17 пикселей.\nСпасибо за понимание!\nПриятной проверки!\nИ успехов в учёбе! ')
-// })();
+/*menu hamburger*/
+
+const burgerItem = document.querySelector('.hamburger');
+const menu = document.querySelector('.burger-menu');
+const menuCloseItem = document.querySelector('.hamburger-close');
+const screenDimming = document.querySelector('.hamburger-screen-dimming');
+const burgerLinks = document.querySelectorAll('.burger-nav-link');
+
+burgerItem.addEventListener('click', () => {
+  menu.classList.add('burger-menu-active');
+  screenDimming.classList.add('hamburger-screen-dimming-active');
+  document.body.style.overflow = 'hidden';
+});
+const closeMenu = () => {
+  menu.classList.remove('burger-menu-active');
+  screenDimming.classList.remove('hamburger-screen-dimming-active');
+  document.body.style.overflow = '';
+};
+
+menuCloseItem.addEventListener('click', closeMenu);
+  
+screenDimming.addEventListener('click', closeMenu);
+
+for (let i = 0; i < burgerLinks.length; i += 1) {
+  burgerLinks[i].addEventListener('click', closeMenu);
+}
+
