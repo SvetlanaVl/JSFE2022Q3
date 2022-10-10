9b1661c6a5313e2df615d79429b16db9466bc0ff
Svetlana <fidelity-67rus@mail.ru>
2022-10-10 13:12:48 +0300

    feat: add testimonials slider

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -25,34 +25,45 @@ for (let i = 0; i < burgerLinks.length; i += 1) {
   burgerLinks[i].addEventListener('click', closeMenu);
 }
 
+// Testimonials slider
 
+const input = document.querySelector('input[type=range]');
+const reviews = document.querySelector('.reviews');
+
+input.addEventListener("input", e => {
+  let sum
+  if(window.matchMedia("(max-width: 1220px)").matches) {
+    sum = input.value * (- 34.9);
+  } else {
+    sum = input.value * (- 25.8);
+  }
+  reviews.classList.add('reviews-active');
+  reviews.style.transform = `translateX(${sum}%)`;
+
+  if(window.matchMedia("(max-width: 1220px)").matches) {
+    let sum = input.value * (- 25.8);
+  }
+});
 
 // Testimonials pop up
 
-const reviews = document.querySelectorAll('.review');
+const reviewsCard = document.querySelectorAll('.review');
 const popUpReview = document.querySelector('.pop-up-review');
 const close = document.querySelector('.pop-up-review-img');
 const popupReviewScreenDimming = document.querySelector('.pop-up-review-screen-dimming');
-const containerPopup = document.querySelector('.container-popup-background');
+const containerPopup = document.querySelector('.container-popup-content');
+const containerPopupScroll = document.querySelector('.container-popup-background');
 const containerRewiewText = document.querySelector('.container-rewiew-text');
 
-
-
-reviews.forEach((element, index) => {
+reviewsCard.forEach((element, index) => {
   element.addEventListener('click', () => {
-    console.log(element)
     popUpReview.classList.add('pop-up-review-active');
     popupReviewScreenDimming.classList.add('pop-up-review-screen-dimming-active');
     document.body.style.overflow = 'hidden';
     containerPopup.innerHTML = element.innerHTML;
+    containerPopupScroll.style.overflow = 'auto';
     if(window.matchMedia("(max-width: 786px)").matches) {
-      containerPopup.style.height = '196%';
-      containerPopup.style.width = '90%';
       containerPopup.style.paddingLeft = '3%';
-      popUpReview.style.left = '20%';
-    } else {
-      containerPopup.style.height = '93%';
-      containerPopup.style.width = '100%';
     }
   });
 });
@@ -61,6 +72,8 @@ const closeReviews = () => {
   popUpReview.classList.remove('pop-up-review-active');
   popupReviewScreenDimming.classList.remove('pop-up-review-screen-dimming-active');
   document.body.style.overflow = '';
+  containerPopup.style.paddingLeft = '';
+  containerPopupScroll.style.overflow = '';
 };
 
 close.addEventListener('click', closeReviews);
