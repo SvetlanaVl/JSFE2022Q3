c720e9d660c7ee2da29f8a061f517e5118e4dbc4
Svetlana <fidelity-67rus@mail.ru>
2022-12-12 20:37:33 +0300

    feat: change proxy server in appLoader

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.up.railway.app/', {
       apiKey: '98d5dce44a9a4d74b8aa39c7c25ce4ee', // получите свой ключ https://newsapi.org/
     });
   }
