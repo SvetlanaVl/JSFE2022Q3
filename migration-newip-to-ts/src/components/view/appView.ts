a616c0977a1118e6d5de3e8a7a593a1bbd292f31
Svetlana <fidelity-67rus@mail.ru>
2022-12-12 18:00:17 +0300

    feat: convert appView.js to appView.ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,18 +1,22 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import type { sourceResponse, newsResponse } from '../../types/interfaceType';
 
 export class AppView {
+  news: News;
+  sources: Sources;
+
   constructor() {
     this.news = new News();
     this.sources = new Sources();
   }
 
-  drawNews(data) {
+  drawNews(data: newsResponse) {
     const values = data?.articles ? data?.articles : [];
     this.news.draw(values);
   }
 
-  drawSources(data) {
+  drawSources(data: sourceResponse) {
     const values = data?.sources ? data?.sources : [];
     this.sources.draw(values);
   }
