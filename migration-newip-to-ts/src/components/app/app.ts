2cb29b0cd4728f2397f2263ccaec555da923654a
Svetlana <fidelity-67rus@mail.ru>
2022-12-12 19:06:20 +0300

    feat: convert app.js to app.ts

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -2,15 +2,19 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+  controller: AppController;
+  view: AppView;
+
   constructor() {
     this.controller = new AppController();
     this.view = new AppView();
   }
 
   start() {
-    document
-      .querySelector('.sources')
-      .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+    const sourcesDocument = document.querySelector('.sources');
+    if(sourcesDocument) {
+      sourcesDocument.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+    }
     this.controller.getSources((data) => this.view.drawSources(data));
   }
 }
